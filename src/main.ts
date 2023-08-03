447023998484b01cedf3ad3ba6aba625d273d306
hadihammurabi <hadihammurabi@gmail.com>
2023-08-03 16:06:31 +0700

    install Primevue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue';
 import routes from './routes';
 import App from './App.vue';
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/lara-light-blue/theme.css';
 import '@/styles/main.css';
 
 createApp(App)
   .use(routes)
+  .use(PrimeVue)
   .mount('#app');
--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+</script>
+
 <template>
   <h1>Not Found</h1>
   <div>
-    <button @click="$router.push('/')">Go to Home</button>
+    <Button @click="$router.push('/')">Go to Home</Button>
   </div>
 </template>
