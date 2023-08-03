e5e3c938699096da4d01302513663effb8bc1c27
hadihammurabi <hadihammurabi@gmail.com>
2023-08-03 16:56:32 +0700

    auto import ts and vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { URL, fileURLToPath } from 'node:url';
+import autoImportComponent from 'unplugin-vue-components/vite';
+import autoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
   plugins: [
     vue(),
+    autoImport({
+      dirs: [
+        './src/composable',
+      ],
+    }),
+    autoImportComponent({
+      dirs: [
+        './src/component',
+        './src/page',
+      ],
+    }),
   ],
   resolve: {
     alias: [
