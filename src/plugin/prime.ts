ecd248c6d686175b65a14c30bc74f07a7fab4f5d
hadihammurabi <hadihammurabi@gmail.com>
2023-08-05 12:43:29 +0700

    refactor: move primevue to plugin

---
+++ src/plugin/prime.ts
@@ -0,0 +1,7 @@
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/lara-light-blue/theme.css';
+import '@/style/main.css';
+
+export const prime = PrimeVue;
