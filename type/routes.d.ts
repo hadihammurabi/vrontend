4347e41cb87be2e4cdd0d41123ad9630c3cab8b1
hadihammurabi <hadihammurabi@gmail.com>
2023-08-05 23:58:19 +0700

    add 404 page

--- type/routes.d.ts
+++ type/routes.d.ts
@@ -40,6 +40,7 @@ import type {
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/[...path]': RouteRecordInfo<'/[...path]', '/:path(.*)', { path: ParamValue<true> }, { path: ParamValue<false> }>,
     '/about/': RouteRecordInfo<'/about/', '/about', Record<never, never>, Record<never, never>>,
     '/about/you': RouteRecordInfo<'/about/you', '/about/you', Record<never, never>, Record<never, never>>,
     '/About': RouteRecordInfo<'/About', '/About', Record<never, never>, Record<never, never>>,
@@ -48,7 +49,6 @@ declare module 'vue-router/auto/routes' {
     '/auth/register': RouteRecordInfo<'/auth/register', '/auth/register', Record<never, never>, Record<never, never>>,
     '/book': RouteRecordInfo<'/book', '/book', Record<never, never>, Record<never, never>>,
     '/book/': RouteRecordInfo<'/book/', '/book', Record<never, never>, Record<never, never>>,
-    '/ErrorNotFound': RouteRecordInfo<'/ErrorNotFound', '/ErrorNotFound', Record<never, never>, Record<never, never>>,
   }
 }
 
