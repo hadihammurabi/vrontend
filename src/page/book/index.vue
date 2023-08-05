0d6f6a0502c6e5d1c5e69db28ac004aaeabd37a9
hadihammurabi <hadihammurabi@gmail.com>
2023-08-06 00:15:04 +0700

    remove manual exec for async state

--- src/page/book/index.vue
+++ src/page/book/index.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
 const bookStore = useBookStore();
-const { execute: loadBooks, state: entries } = bookStore.all();
-
-onBeforeMount(() => {
-  loadBooks();
-});
+const { state: entries } = bookStore.all();
 </script>
 
 
 <template>
   <h1>Book Table</h1>
   <code>
-    {{ entries  }}
+    {{ entries }}
   </code>
 </template>
